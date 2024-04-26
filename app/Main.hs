e47811391fd3f3a49beeae2ac1ee86286a1b29e0
Tomek Grzesiak <tomek.grzesiak@hotmail.com>
2024-04-26 20:19:22 +0200

    Has ctx

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import Data.Aeson
 import Lib (test)
-import Protolude
+import Protolude hiding (ask)
 import TextShow
 import TextShow.Generic
 import Control.Monad.Except.CoHas (CoHas, inject)
+import Control.Monad.Reader.Has
 
 main :: IO ()
 main = do
@@ -14,21 +15,36 @@ main = do
 
 data Err1 = Err1 Text 
     deriving (Generic, ToJSON, FromJSON)
-    deriving TextShow  via FromGeneric  (Err1)
+    deriving TextShow  via FromGeneric  Err1
 
 data Err2 = Err2
   { err1 :: Err1
   }
   deriving (Generic, ToJSON, FromJSON, CoHas Err1)
-  deriving TextShow  via FromGeneric  (Err2)
+  deriving TextShow  via FromGeneric  Err2
 
-data Ctx = Ctx
+data Ctx = Ctx {
+    ctx1 :: Ctx1
+  , ctx2 :: Ctx2
+} deriving (Generic, Has Ctx1, Has Ctx2)
+
+data Ctx1 = Ctx1
+  deriving (Generic, ToJSON, FromJSON)
+  deriving TextShow  via FromGeneric  Ctx1
+data Ctx2 = Ctx2
+  deriving (Generic, ToJSON, FromJSON)
+  deriving TextShow  via FromGeneric  Ctx2
+
 
 test1 :: ExceptT Err2 (ReaderT Ctx IO) ()
 test1 = undefined
 
-test2 :: (MonadError  e m, CoHas Err1 e , MonadReader Ctx m) => m ()
-test2 = throwError . inject $ Err1 "err1"
+class HasCtx
+
+test2 :: (Has Ctx1 ctx, MonadError e m, CoHas Err1 e , MonadReader ctx m) => m ()
+test2 = do
+  ctx :: Ctx1 <- ask
+  throwError . inject $ Err1 "err1"
 
 test3 :: ExceptT Err2 (ReaderT Ctx IO) ()
 test3 = do
