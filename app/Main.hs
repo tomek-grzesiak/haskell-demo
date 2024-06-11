a91ce6bbb46a2d296079c8f69699544d1c45d7d6
Tomek Grzesiak <tomek.grzesiak@hotmail.com>
2024-06-11 13:06:26 +0200

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,52 +1,79 @@
-module Main where
+{-# LANGUAGE DefaultSignatures, DeriveGeneric, TypeOperators, FlexibleContexts #-}
 
-import Data.Aeson
-import Lib (test)
-import Protolude hiding (ask)
-import TextShow
-import TextShow.Generic
-import Control.Monad.Except.CoHas (CoHas, inject)
-import Control.Monad.Reader.Has
+import GHC.Generics
+import Data.Bits
+import Protolude hiding (put, get)
 
-main :: IO ()
+data Bit = O | I deriving Show
+
+class Serialize a where
+  put :: a -> [Bit]
+
+  default put :: (Generic a, GSerialize (Rep a)) => a -> [Bit]
+  put a = gput (from a)
+
+  get :: [Bit] -> (a, [Bit])
+
+  default get :: (Generic a, GSerialize (Rep a)) => [Bit] -> (a, [Bit])
+  get xs = (to x, xs')
+    where (x, xs') = gget xs
+
+class GSerialize f where
+  gput :: f a -> [Bit]
+  gget :: [Bit] -> (f a, [Bit])
+
+-- | Unit: used for constructors without arguments
+instance GSerialize U1 where
+  gput U1 = []
+  gget xs = (U1, xs)
+
+-- | Products: encode multiple arguments to constructors
+instance (GSerialize a, GSerialize b) => GSerialize (a :*: b) where
+  gput (a :*: b) = gput a ++ gput b
+  gget xs = (a :*: b, xs'')
+    where (a, xs') = gget xs
+          (b, xs'') = gget xs'
+
+-- | Sums: encode choice between constructors
+instance (GSerialize a, GSerialize b) => GSerialize (a :+: b) where
+  gput (L1 x) = O : gput x
+  gput (R1 x) = I : gput x
+  gget (O:xs) = (L1 x, xs')
+    where (x, xs') = gget xs
+  gget (I:xs) = (R1 x, xs')
+    where (x, xs') = gget xs
+
+-- | Meta-information (constructor names, etc.)
+instance (GSerialize a) => GSerialize (M1 i c a) where
+  gput (M1 x) = gput x
+  gget xs = (M1 x, xs')
+    where (x, xs') = gget xs
+
+-- | Constants, additional parameters and recursion of kind *
+instance (Serialize a) => GSerialize (K1 i a) where
+  gput (K1 x) = put x
+  gget xs = (K1 x, xs')
+    where (x, xs') = get xs
+
+instance Serialize Bool where
+  put True = [I]
+  put False = [O]
+  get (I:xs) = (True, xs)
+  get (O:xs) = (False, xs)
+
+--
+-- Try it out. (Normally this would be in a separate module.)
+--
+
+data UserTree a = Node a (UserTree a) (UserTree a) | Leaf
+  deriving (Generic, Show)
+
+instance (Serialize a) => Serialize (UserTree a)
+
 main = do
-  -- void $ flip runReaderT Ctx2 $ runExceptT test3
-  print $ encode $ Err2 . Err1 $ "Hello, Haskell!"
-
-data Err1 = Err1 Text 
-    deriving (Generic, ToJSON, FromJSON)
-    deriving TextShow  via FromGeneric  Err1
-
-data Err2 = Err2
-  { err1 :: Err1
-  }
-  deriving (Generic, ToJSON, FromJSON, CoHas Err1)
-  deriving TextShow  via FromGeneric  Err2
-
-data Ctx = Ctx {
-    ctx1 :: Ctx1
-  , ctx2 :: Ctx2
-} deriving (Generic, Has Ctx1, Has Ctx2)
-
-data Ctx1 = Ctx1
-  deriving (Generic, ToJSON, FromJSON)
-  deriving TextShow  via FromGeneric  Ctx1
-data Ctx2 = Ctx2
-  deriving (Generic, ToJSON, FromJSON)
-  deriving TextShow  via FromGeneric  Ctx2
-
-
-test1 :: ExceptT Err2 (ReaderT Ctx IO) ()
-test1 = undefined
-
-class HasCtx
-
-test2 :: (Has Ctx1 ctx, MonadError e m, CoHas Err1 e , MonadReader ctx m) => m ()
-test2 = do
-  ctx :: Ctx1 <- ask
-  throwError . inject $ Err1 "err1"
-
-test3 :: ExceptT Err2 (ReaderT Ctx IO) ()
-test3 = do
-  test1
-  test2
+  let xs = put True
+  print (fst . get $ xs :: Bool)
+  let ys = put (Leaf :: UserTree Bool)
+  print (fst . get $ ys :: UserTree Bool)
+  let zs = put (Node False Leaf Leaf :: UserTree Bool)
+  print (fst . get $ zs :: UserTree Bool)
